d94df4f12e3bf00a773e089293a56e12f061fbf8
Barbara Altares <b.altares@gmail.com>
2021-11-13 16:45:24 +0100

    Deshacer TODO Firebase, MAL!!

---
+++ src/app/core/models/plant-data.model.ts
@@ -0,0 +1,21 @@
+export interface PlantDataModel {
+  dateCreation: string;
+  graphicIntervalList: GraphicIntervalList[];
+  growingInterval: number;
+  image: string;
+  nameCommon: string;
+  nameScientific: string;
+  needSun: number;
+  needWater: number;
+  txtCulinary: string;
+  txtDescription: string;
+  txtResume: string;
+  txtWikiLink: string;
+  type: string;
+}
+
+interface GraphicIntervalList {
+  finish: string;
+  start: string;
+  type: string;
+}
---
+++ src/app/shared/components/tool-filter/tool-filter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tool-filter',
+  templateUrl: './tool-filter.component.html',
+  styleUrls: ['./tool-filter.component.scss']
+})
+export class ToolFilterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
