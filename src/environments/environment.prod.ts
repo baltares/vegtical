5fdf64059aeb377fdd26ad3991c1849b7c257fb5
Barbara Altares <b.altares@gmail.com>
2021-12-06 14:18:58 +0100

    compilado

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,20 @@
+import { domain, clientId } from '../../auth_config.json';
+
 export const environment = {
-  production: true
+  production: true,
+  auth: {
+    domain,
+    clientId,
+    redirectUri: window.location.origin,
+  },
+  firebaseConfig: {
+    apiKey: "AIzaSyBfRJsndqO1ESOHu6fRUAEZ8SEnaLQbat4",
+    authDomain: "vegticaldb.firebaseapp.com",
+    databaseURL: "https://vegticaldb-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "vegticaldb",
+    storageBucket: "vegticaldb.appspot.com",
+    messagingSenderId: "375056728121",
+    appId: "1:375056728121:web:fe4be6a0c50c9522170de8",
+    measurementId: "G-MFEL4F9JWK"
+  }
 };
